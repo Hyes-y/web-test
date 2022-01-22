9b3081212361d7cee0e2c7ecb996d277f81ca082
Hyes-y <55697800+Hyes-y@users.noreply.github.com>
2022-01-22 17:52:46 +0900

    엘리스 2주차 과제

---
+++ index.js
@@ -0,0 +1,8 @@
+function divOnOff(element) {
+    var panel = element.nextElementSibling;
+    if (panel.style.display === "block") {
+        panel.style.display = "none";
+    } else {
+        panel.style.display = "block";
+    }
+};
